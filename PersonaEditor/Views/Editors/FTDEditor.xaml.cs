4a628ed8c3a3d8a6bf14ed4425a25dda565a8de1
Meloman19 <meloman19ru@gmail.com>
2021-03-18 22:58:36 +0300

    FTD string copy

--- PersonaEditor/Views/Editors/FTDEditor.xaml.cs
+++ PersonaEditor/Views/Editors/FTDEditor.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using PersonaEditor.ViewModels.Editors;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace PersonaEditor.Views.Editors
 {
@@ -7,6 +9,17 @@ namespace PersonaEditor.Views.Editors
         public FTDEditor()
         {
             InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var vm = (sender as MenuItem)?.CommandParameter as FTDSingleVM;
+            if (vm == null)
+            {
+                return;
+            }
+
+            Clipboard.SetText(vm.DataDecode.Replace('\0',' '));
         }
     }
 }
